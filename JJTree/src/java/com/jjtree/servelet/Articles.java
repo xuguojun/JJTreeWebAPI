0dda1407c4bfd79ff42a5ded11f10c0c574c28c2
guojunxu <gjxu@kaikeba.com>
2015-08-23 01:50:38 +0800

    1 add const 2 format code

--- JJTree/src/java/com/jjtree/servelet/Articles.java
+++ JJTree/src/java/com/jjtree/servelet/Articles.java
@@ -30,6 +30,20 @@ import org.json.JSONObject;
 @WebServlet(name = "Articles", urlPatterns = {"/articles"})
 public class Articles extends HttpServlet {
 
+    private static final String TOP = "top";
+    private static final String RECENT = "recent";
+    private static final String COLLECTION = "collection";
+    private static final String EDIT = "edit";
+    private static final String REWARDED = "rewarded";
+    private static final String REWARD = "reward";
+    private static final String USEFUL = "useful";
+    private static final String USELESS = "useless";
+    private static final String SHARE = "share";
+    private static final String FILTER_READ = "read";
+    private static final String FILTER_UNREAD = "unread";
+    private static final String FILTER_USEFUL = "useful";
+    private static final String FILTER_USELESS = "useless";
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -42,12 +56,12 @@ public class Articles extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/json");
-        
+
     }
 
     private Connection conn;
     private Statement stmt;
-    
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -61,24 +75,24 @@ public class Articles extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        
+
         // one single article
         String pathInfo = request.getPathInfo();
-        
+
         int singleArticleID = -1;
-        if (pathInfo != null){
+        if (pathInfo != null) {
             String[] path = pathInfo.split("/");
             singleArticleID = Integer.parseInt(path[1]);
         }
-        
+
         // mutiple articles
         String category = request.getParameter("category");
         int pageSize = 0;
         int pageIndex = 0;
-        
-        if (category != null){
+
+        if (category != null) {
             pageSize = Integer.parseInt(request.getParameter("pageSize"));
-            pageIndex = Integer.parseInt(request.getParameter("pageIndex"));    
+            pageIndex = Integer.parseInt(request.getParameter("pageIndex"));
         }
 
         try {
@@ -91,26 +105,26 @@ public class Articles extends HttpServlet {
             // Execute SQL query
             stmt = conn.createStatement();
             String sql = null;
-            
-            if (singleArticleID >= 0){
+
+            if (singleArticleID >= 0) {
                 sql = "SELECT * FROM JArticle WHERE articleID = " + singleArticleID;
             }
-            
+
             if (category != null) {
-                if (category.equalsIgnoreCase("top")){
+                if (category.equalsIgnoreCase(TOP)) {
                     sql = "SELECT * FROM JArticle ORDER BY usefulValue DESC OFFSET " + pageSize * pageIndex + " ROWS FETCH NEXT " + pageSize + " ROWS ONLY";
                 }
-                
-                if (category.equalsIgnoreCase("recent")) {
+
+                if (category.equalsIgnoreCase(RECENT)) {
                     sql = "SELECT * FROM JArticle ORDER BY createdAt DESC OFFSET " + pageSize * pageIndex + " ROWS FETCH NEXT " + pageSize + " ROWS ONLY";
                 }
             }
-            
+
             ResultSet rs = stmt.executeQuery(sql);
-            
+
             JSONObject articlesObject = new JSONObject();
             JSONArray articles = new JSONArray();
-            
+
             JSONArray paragraphs = new JSONArray();
 
             // Extract data from result set
@@ -121,10 +135,10 @@ public class Articles extends HttpServlet {
 
                 boolean isPrivate = rs.getBoolean("isPrivate");
                 String title = rs.getString("title");
-                
+
                 Timestamp createdAt = rs.getTimestamp("createdAt");
                 Timestamp updatedAt = rs.getTimestamp("updatedAt");
-                
+
                 int usefulValue = rs.getInt("usefulValue");
                 int uselessValue = rs.getInt("uselessValue");
                 int viewCount = rs.getInt("viewCount");
@@ -132,66 +146,66 @@ public class Articles extends HttpServlet {
                 String accountUrl = "/accounts/" + authorID;
                 JSONObject author = JServeletManager.fetchFrom(request, accountUrl);
                 JSONObject article = new JSONObject();
-                
+
                 Statement subStatement = conn.createStatement();
                 String subSql = "SELECT * FROM JParagraph WHERE articleID = " + articleID;
                 ResultSet subRs = subStatement.executeQuery(subSql);
-                
-                while(subRs.next()){
+
+                while (subRs.next()) {
                     int paragraphID = subRs.getInt("paragraphID");
                     int position = subRs.getInt("position");
-                    
+
                     String type = subRs.getString("type");
                     String content = subRs.getString("content");
-                    
+
                     JSONObject paragraph = new JSONObject();
-                    
+
                     paragraph.put("paragraphID", paragraphID);
                     paragraph.put("position", position);
-                    
+
                     paragraph.put("type", type);
                     paragraph.put("content", content);
-                    
+
                     paragraphs.put(paragraph);
                 }
 
                 article.put("articleID", articleID);
                 article.put("authorID", authorID);
-                
+
                 article.put("isPrivate", isPrivate);
                 article.put("title", title);
-                
+
                 article.put("createdAt", createdAt);
                 article.put("updatedAt", updatedAt);
-                
+
                 article.put("usefulValue", usefulValue);
                 article.put("uselessValue", uselessValue);
                 article.put("viewCount", viewCount);
-                
+
                 article.put("paragraphs", paragraphs);
-                
+
                 PrintWriter writer = response.getWriter();
-                
+
                 // single article 
-                if (singleArticleID >= 0){
+                if (singleArticleID >= 0) {
                     article.put("author", author);
                     writer.print(article);
                     writer.flush();
                 }
-                
-                if (category != null){
+
+                if (category != null) {
                     article.put("author", author);
                     articles.put(article);
                 }
             }
 
-            if (category != null){
+            if (category != null) {
                 articlesObject.put("articles", articles);
                 PrintWriter writer = response.getWriter();
                 writer.print(articlesObject);
-                writer.flush();  
+                writer.flush();
             }
-      
+
             // Clean-up environment
             rs.close();
             stmt.close();
