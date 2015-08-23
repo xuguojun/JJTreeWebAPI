619f5c57bfef68aef694da1757d449e33c8043c6
guojunxu <gjxu@kaikeba.com>
2015-08-23 12:27:00 +0800

    implement login interface

---
+++ JJTree/src/java/com/jjtree/servelet/Login.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jjtree.servelet;
+
+import com.jjtree.utilities.JConstant;
+import com.jjtree.utilities.JConverter;
+import com.jjtree.utilities.JServeletManager;
+import com.jjtree.utilities.JString;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author rose
+ */
+public class Login extends HttpServlet {
+
+    private Connection conn;
+    private Statement stmt;
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        JSONObject jsonObject = JConverter.convert(request);
+        if (jsonObject == null) {
+            return;
+        }
+
+        String account = null;
+        String password = null;
+        try {
+            account = jsonObject.getString("account");
+            password = jsonObject.getString("password");
+
+            if (account == null || password == null) {
+                return;
+            }
+        } catch (JSONException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            // Register JDBC driver
+            Class.forName(JConstant.JDBC_DRIVER);
+
+            // Open a connection
+            conn = DriverManager.getConnection(JConstant.DB_URL, JConstant.USER, JConstant.PASSWORD);
+
+            // Execute SQL query
+            stmt = conn.createStatement();
+            String sql = null;
+            
+            boolean isPhone = JString.isPhoneNumber(account);
+            if(isPhone){
+                sql = "SELECT * FROM JUser WHERE mobile = '" + account+"'";
+            }
+            
+            boolean isEmail = JString.isEmail(account);
+            if(isEmail){
+                sql = "SELECT * FROM JUser WHERE email = '" + account+"'";
+            }
+            
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+                int accountID = rs.getInt("userID");
+
+                String pass = rs.getString("password");
+               
+                if (password != null){
+                    if(password.equals(pass)){
+                        String accountUrl = "/accounts/" + accountID;
+                        JSONObject accountObject = JServeletManager.fetchFrom(request, accountUrl);
+                        
+                        PrintWriter writer = response.getWriter();
+                        writer.print(accountObject);
+                        writer.flush();
+                    }
+                }
+            }
+
+            // Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        } //end try
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
